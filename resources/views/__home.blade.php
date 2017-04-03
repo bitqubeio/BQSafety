f947277b5e75c3a00841c49a86d1ac31e51571ed
Eddy Loayza <Eddy Loayza>
2017-04-03 10:51:29 -0500

    03-04-2017 respaldo antes de formatear

--- resources/views/__home.blade.php
+++ resources/views/__home.blade.php
@@ -7,9 +7,9 @@
                 <img src="{{ url('/bqsafety/img/user.png') }}" alt="" style="width: 100px">
                 <h2 class="mt-3">¡Bienvenido(a), {{ Auth::user()->user_username }}!</h2>
                 @if(Auth::user()->user_status)
-                    <p>"No hay trabajo tan urgente que no pueda ser ejecutado con seguridad". </p>
+                    <p>"No hay trabajo tan urgente que no pueda ser ejecutado con seguridad". :) </p>
                 @else
-                    <p>En breve el administrador del sistema validará su registro. </p>
+                    <p>En breve el administrador del sistema validará su registro. :) </p>
                 @endif
             </div>
         </div>
